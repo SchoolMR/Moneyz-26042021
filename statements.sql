082d860d1327519aea872eed98d6802a74819aaf
mikereule <mikereuleprive@gmail.com>
2021-04-26 13:20:14 +0200

    Commit statements.sql

---
+++ statements.sql
@@ -0,0 +1,31 @@
+-- Create en start Database
+
+CREATE DATABASE moneyz;
+use moneyz;
+
+-- Tabellen
+
+CREATE TABLE tblUser
+(
+
+    id INT AUTO_INCREMENT NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE tblMoneyz
+(
+
+    id INT AUTO_INCREMENT NOT NULL,
+    user_id INT NOT NULL,
+    moneyz INT,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES tblUser(id)
+
+);
